e61d6555b317ecc0bc66d85d5cedf1b4655b49a1
leonardoguido3 <leonardoguido3@gmail.com>
2023-01-05 11:33:37 -0300

    Inicio do projeto

---
+++ Fluxo.cs
@@ -0,0 +1,49 @@
+﻿using ExemploTemplateConsole.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploTemplateConsole
+{
+    public class Fluxo
+    {
+        public void Executar()
+        {
+            // declarei uma variável do tipo pessoa
+            // adicionei o nome da variavei como pessoa
+            // inicializei usando o igual
+            // recebendo o valor instanciado atraves do new
+            // que o new instanciou a classe Pessoa
+            // fiz com que a variavel pessoa tenha todos os atributos da classe
+            Pessoa pessoa = new Pessoa();
+
+            // quando eu coloco o nome da variavel que eu instanciei um objeto (classe)
+            // atraves do "." consigo acessar e modificar os atributos que tem na classe
+            // ou seja, o nome está dentro de class Pessoa
+            // public string Nome { get; set; }
+            pessoa.Nome = "Leonardo";
+
+            // aqui converti uma data para datetime
+            // vou informar uma data fake para fingir ser meu aniversario
+            pessoa.Nascimento = DateTime.Parse("2020-01-01");
+
+            TimeSpan tempoEntreDatas = DateTime.Now - pessoa.Nascimento;
+
+            DateTime quantosAnosTem = new DateTime(tempoEntreDatas.Ticks);
+
+            int quantidadeAnos = quantosAnosTem.Year - 1;
+
+            if(quantidadeAnos < 18)
+            {
+                Console.WriteLine("Menor de idade");
+            }
+            else
+            {
+                Console.WriteLine("Maior de idade");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploTemplateConsole.Entidades;
+
+namespace ExemploTemplateConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fluxo fluxo = new Fluxo();
+            fluxo.Executar();
+        }
+    }
+}
